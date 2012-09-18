5759c92da5a04185001b8276cd94951a1786ccfa
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-09-18 18:12:12 -0300

    Making Policy an event queue

--- lib/permit/policy.rb
+++ lib/permit/policy.rb
@@ -1,11 +1,11 @@
 module Permit
   class Policy
     include Enumerable
-    attr_reader :rules
+    attr_reader :rules_events
 
     def initialize(opts={})
       @resource_id = opts[:resource_id]
-      @rules = []
+      @rules_events = []
       @producer = opts[:producer]
     end
 
@@ -13,58 +13,43 @@ module Permit
       subject_id = rule[:subject_id]
       actions = rule[:action].respond_to?(:each) ? rule[:action] : [rule[:action]]
 
-      if i = index_of(subject_id)
-        actions.each do |a|
-          @rules[i][:actions][a.to_sym] = true
-        end
-      else
-        init = { :subject_id => subject_id, :actions => {} }
-        new_rule = actions.inject(init) do |acc, a|
-          acc[:actions][a.to_sym] = true
-          acc
-        end
-        @rules << new_rule
+      new_rule = { :resource_id => @resource_id,
+                   :subject_id => subject_id, :actions => {} }
+      new_rule = actions.reduce(new_rule) do |acc, a|
+        acc[:actions][a.to_sym] = true
+        acc
       end
+
+      @rules_events << Event.new(:name => "create", :payload => new_rule)
     end
 
     def remove(opts)
       subject_id = opts[:subject_id]
-      filter = {
-        :action => []
-      }.merge(opts)
-      actions = filter[:action].respond_to?(:each) ? filter[:action] : [filter[:action]]
+      new_rule = if action = opts[:action]
+        actions = action.respond_to?(:each) ? action : [action]
 
-      return nil unless index_of(subject_id)
-      return delete_rule(subject_id) if actions.length == 0
-
-
-      rule_idx = index_of(subject_id)
-      actions.each do |a|
-        @rules[rule_idx][:actions].delete(a.to_sym)
+        new_rule = { :resource_id => @resource_id,
+                     :subject_id => subject_id, :actions => {} }
+        actions.reduce(new_rule) do |acc, a|
+          acc[:actions][a.to_sym] = true
+          acc
+        end
+      else
+       { :resource_id => @resource_id, :subject_id => subject_id }
       end
-      delete_rule(subject_id) if @rules[rule_idx][:actions].empty?
+
+      @rules_events << Event.new(:name => "remove", :payload => new_rule)
     end
 
     def commit
-      @producer.publish(self)
-    end
-
-    def each(&block)
-      @rules.each do |rule|
-        rule[:resource_id] = @resource_id
-        block.call(rule)
+      @rules_events.each do |event|
+        @producer.publish(self)
       end
     end
 
-    protected
-
-    def index_of(subject_id)
-      @rules.index { |r| r[:subject_id] == subject_id }
-    end
-
-    def delete_rule(subject_id)
-      if i = index_of(subject_id)
-        @rules.delete_at(i)
+    def each(&block)
+      @rules_events.each do |rule|
+        block.call(rule)
       end
     end
   end
--- spec/policy_spec.rb
+++ spec/policy_spec.rb
@@ -7,101 +7,97 @@ module Permit
       subject.should be_a Policy
     end
 
-    it "should respond to #add" do
-      subject.should respond_to :add
-    end
+    context "#add" do
+      it "should respond to #add" do
+        subject.should respond_to :add
+      end
 
-    it "should increment rules" do
-      subject
-      expect {
-        subject.add(:subject_id => "s", :action => :a)
-      }.to change(subject.rules, :length).by(1)
-    end
+      it "should increment rules event list" do
+        subject
+        expect {
+          subject.add(:subject_id => "s", :action => :a)
+        }.to change(subject.rules_events, :length).by(1)
+      end
 
-    it "should add the rules" do
-      rule = { :subject_id => "s", :action => :a }
-      subject.add(rule)
-
-      rules = subject.rules
-      rules.should include({ :subject_id => 's', :actions => { :a => true } })
-    end
-
-    it "should accept a list of actions" do
-      subject
-      rule = { :subject_id => "s", :action => [:a1, :a2] }
-      expect {
+      it "should add the rules" do
+        rule = { :subject_id => "s", :action => :a }
         subject.add(rule)
-      }.to change(subject.rules, :length).by(1)
+
+        rules = subject.rules_events
+        rules.last.name.should == "create"
+        rules.last.payload.should == \
+          { :resource_id => "r", :subject_id => "s", :actions => { :a => true } }
+      end
+
+      it "should accept a list of actions" do
+        subject
+        rule = { :subject_id => "s", :action => [:a1, :a2] }
+        expect {
+          subject.add(rule)
+        }.to change(subject.rules_events, :length).by(1)
+      end
     end
 
-    it "should respond to #remove" do
-      subject.should respond_to :remove
-    end
+    context "#remove" do
+      it "should respond to #remove" do
+        subject.should respond_to :remove
+      end
 
-    it "should remove rules passing the subject" do
-      rule = { :subject_id => "s", :action => [:a1, :a2] }
-      subject.add(rule)
-      subject.add(:subject_id => "t", :action => :a1)
+      it "should remove rules passing the subject" do
+        rule = { :subject_id => "s", :action => [:a1, :a2] }
+        subject.add(rule)
+        subject.add(:subject_id => "t", :action => :a1)
 
-      expect {
         subject.remove(:subject_id => "s")
-      }.to change(subject.rules, :length).by(-1)
+        subject.rules_events.length.should == 3
+        subject.rules_events.last.name == "remove"
+      end
+
+      it "should remove the rules passing the subject and action" do
+        rule = { :subject_id => "s", :action => [:a1, :a2] }
+        subject.add(rule)
+        subject.add(:subject_id => "t", :action => :a1)
+
+        subject.remove(:subject_id => "s", :action => :a1)
+        rules = subject.rules_events
+
+        rules.should_not include({ :subject_id => "s", :action => :a1 })
+      end
+
+      it "should remove the rules passing a list of actions" do
+        rule = { :subject_id => "s", :action => [:a1, :a2] }
+        subject.add(rule)
+        subject.add(:subject_id => "t", :action => :a1)
+
+        expect {
+          subject.remove(:subject_id => "s", :actions => [:a1, :a2])
+        }.to change(subject.rules_events, :length).by(1)
+      end
+
     end
 
-    it "should remove the rules passing the subject and action" do
-      rule = { :subject_id => "s", :action => [:a1, :a2] }
-      subject.add(rule)
-      subject.add(:subject_id => "t", :action => :a1)
+    context "#commit" do
+      it "should respond to #commit" do
+        Policy.new(:resource_id => "r").should respond_to :commit
+      end
 
-      subject.remove(:subject_id => "s", :action => :a1)
-      rules = subject.rules
+      it "should commit rules" do
+        producer = double('Producer')
+        producer.stub(:publish) { |policy| nil }
+        p = Policy.new(:resource_id => "r", :producer => producer)
+        p.add({ :subject_id => "s", :action => :a })
+        p.remove({ :subject_id => "s", :action => :a })
 
-      rules.should_not include({ :subject_id => "s", :action => :a1 })
-    end
-
-    it "should not remove all the rules when passing specific action" do
-      rule = { :subject_id => "s", :action => [:a1, :a2] }
-      subject.add(rule)
-      subject.add(:subject_id => "t", :action => :a1)
-
-      subject.remove(:subject_id => "s", :action => :a1)
-      rules = subject.rules
-
-      rules.should include({ :subject_id => "s", :actions => {:a2 => true} })
-    end
-
-    it "should remove the rules passing a list of actions" do
-      rule = { :subject_id => "s", :action => [:a1, :a2] }
-      subject.add(rule)
-      subject.add(:subject_id => "t", :action => :a1)
-
-      expect {
-        subject.remove(:subject_id => "s", :actions => [:a1, :a2])
-      }.to change(subject.rules, :length).by(-1)
-    end
-
-    it "should respond to #commit" do
-      Policy.new(:resource_id => "r").should respond_to :commit
-    end
-
-    it "should commit rules" do
-      producer = double('Producer')
-      producer.stub(:publish) { |policy| nil }
-      p = Policy.new(:resource_id => "r", :producer => producer)
-      p.add({ :subject_id => "s", :action => :a })
-
-      producer.should_receive(:publish).with(p)
-      p.commit
+        producer.should_receive(:publish).with(p).twice
+        p.commit
+      end
     end
 
     it "should be iterable" do
       subject.add(:subject_id => "s1", :action => [:a1, :a2])
       subject.add(:subject_id => "s2", :action => [:a2])
 
-      rules = [ {:subject_id => "s", :resource_id => "r1", :actions => { :a1 => true, :a2 => true }},
-                {:subject_id => "s", :resource_id => "r1", :actions => { :a2 => true } } ]
-
-      subject.to_a.collect(&:to_set) == rules.to_a.collect(&:to_set)
+      subject.to_a.to_set == subject.rules_events.to_set
     end
   end
 end
