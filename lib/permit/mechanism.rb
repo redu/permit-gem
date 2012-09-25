e41cc7b51799bd7afb1bbb149f596bcf443ccc3d
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-09-25 09:59:12 -0300

    Refactoring Mechanism#able_to? to take into account action precedence: whenever the subject is able to manage he may also read, destroy, update, index and create

--- lib/permit/mechanism.rb
+++ lib/permit/mechanism.rb
@@ -10,14 +10,44 @@ module Permit
       }.merge(opts)
     end
 
-    def able_to?(action, resource)
-      response = head(:action => action, :resource_id => resource)
-      response.status == 200
+    # Queries the server side component for the rules of some resource for
+    # the specified subject.
+    #
+    #   mech = Permit::Mechanism.new(:subject_id => "core:user_1")
+    #   mech.able_to?(:read, "core:course_2")
+    #   => true
+    #
+    # The Mechanism also takes into accout the actions precedence. For example
+    # when the subject is enabled to manage some resource, asking for reading
+    # rights the result is true. This behaivor may be bypassed using the
+    # argument :strict => true
+    def able_to?(action, resource, opts={})
+      if opts[:strict]
+        response = head(:action => action, :resource_id => resource)
+        response.status == 200
+      else
+        response = get(:resource_id => resource)
+        return false unless response.success?
+        data = JSON.parse(response.body, :symbolize_keys => true)
+
+        if rule = data[0]
+          if %w(read create destroy index update).include?(action.to_s)
+            return rule[:actions][:manage] == true
+          else
+            return rule[:actions][action] == true
+          end
+        end
+      end
     end
 
     def head(opts)
       params = opts.merge({:subject_id => @config[:subject_id]})
       connection.head("/rules", params)
+    end
+
+    def get(opts)
+      params = opts.merge({:subject_id => @config[:subject_id]})
+      connection.get("/rules", params)
     end
 
     protected
--- spec/mechanism_spec.rb
+++ spec/mechanism_spec.rb
@@ -15,27 +15,75 @@ module Permit
       subject.should respond_to :able_to?
     end
 
-    it "should generate an HEAD request with the correct params" do
-      stub_request(:head, "http://permit.redu.com.br/rules?action=read&resource_id=r&subject_id=s").
-        with(:headers => {'Accept'=>'application/json', 'Expect'=>''}).
-        to_return(:status => 200, :body => "", :headers => {})
+    context "strict request" do
+      let(:host) do
+        "http://permit.redu.com.br/rules?action=read&resource_id=r" + \
+          "&subject_id=s"
+      end
+      before do
+        stub_request(:head, host).
+          with(:headers => {'Accept'=>'application/json', 'Expect'=>''}).
+          to_return(:status => 200)
+      end
 
-      response = subject.head(:resource_id => "r", :action => :read)
-      response.status.should == 200
+      it "should generate an HEAD request with the correct params" do
+        response = subject.head(:resource_id => "r", :action => :read)
+        response.status.should == 200
+      end
+
+      it "should allow access when response code is 200" do
+        subject.should be_able_to(:read, "r", :strict => true)
+      end
+
+      it "should not allow access when response code is 404" do
+        stub_request(:head, host).to_return(:status => 404, :body => "")
+        subject.should_not be_able_to(:read, "r", :strict => true)
+      end
     end
 
-    it "should allow access when response code is 200" do
-      stub_request(:head, "http://permit.redu.com.br/rules?action=read&resource_id=r&subject_id=s").
-        to_return(:status => 200, :body => "")
+    context "non strict request" do
+      before do
+        h = "http://permit.redu.com.br/rules?resource_id=r&subject_id=s"
+        resp = [{ :id => "2", :subject_id => "s", :resource_id => "r",
+                  :actions => { :manage => true, :foo => true } }].to_json
+        stub_request(:get, h).
+          with(:headers => {'Accept'=>'application/json'}).
+          to_return(:status => 200, :body => resp,
+                    :headers => { 'Contet-type' => 'application/json' })
+      end
 
-      subject.should be_able_to(:read, "r")
-    end
+      %w(read create destroy index update manage).each do |action|
+        it "should respect the precedence of manage over #{action}" do
+          subject.should be_able_to(action.to_sym, "r")
+        end
+      end
 
-    it "should not allow access when response code is 404" do
-      stub_request(:head, "http://permit.redu.com.br/rules?action=read&resource_id=r&subject_id=s").
-        to_return(:status => 404, :body => "")
+      it "should work for custom actions" do
+        subject.should be_able_to(:foo, "r")
+      end
 
-      subject.should_not be_able_to(:read, "r")
+      it "should not allow access when the action is not specified" do
+        subject.should_not be_able_to(:preview, "r")
+      end
+
+      it "should not allow access when the HTTP status is 404" do
+        host = "http://permit.redu.com.br/rules?resource_id=r&subject_id=s"
+        stub_request(:get, host).
+          with(:headers => {'Accept'=>'application/json'}).
+          to_return(:status => 404)
+
+        subject.should_not be_able_to(:read, "r")
+      end
+
+      it "should not allow access when the response is empty" do
+        h = "http://permit.redu.com.br/rules?resource_id=r&subject_id=s"
+        stub_request(:get, h).
+          with(:headers => {'Accept'=>'application/json'}).
+          to_return(:status => 200, :body => [].to_json,
+                    :headers => { 'Contet-type' => 'application/json' })
+
+        subject.should_not be_able_to(:read, "r")
+      end
     end
   end
 end
